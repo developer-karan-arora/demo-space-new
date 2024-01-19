12a07e769aa698fc5722f33f72beb27b481a1945
karan arora <ka1319@dseu.ac.in>
2024-01-19 16:25:59 +0530

    added space-image

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 import "./Game.css";
 
 function App() {
-  return <div className="game-bg"></div>;
+  return (
+    <div className="game-bg">
+      <div className="game">
+        <div className="game-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

c3f83da346cfc386ad6f79ed21d6d0a5ef91135c
karan arora <ka1319@dseu.ac.in>
2024-01-19 16:34:58 +0530

    added game-text-ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import "./Game.css";
 
 function App() {
+  // using states 
+  let [missfire, setMissfire] = useState(0);
+  let [currentWave, setCurrentWave] = useState(0);
+
+
   return (
     <div className="game-bg">
       <div className="game">
         <div className="game-container">
           <div className="game-grid-animation"></div>
-          <div></div>
+          <div className="data-container">
+            <p>Press any Key to Start Game and space to go to next level</p>
+            <p>{"CURRENT WAVE : " + currentWave}</p>
+            <p>{"MISSFIRE : " + missfire}</p>
+          </div>
         </div>
       </div>
     </div>
